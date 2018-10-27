a3500f2e59707fc9266945bef7af2070601969ff
Gheorghe <gheorghe.chirt@yahoo.com>
2018-10-27 15:56:03 +0300

    Initial project

---
+++ ClassRegister/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClassRegister.MvcApplication" Language="C#" %>
---
+++ ClassRegister/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClassRegister.Startup))]
+namespace ClassRegister
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
